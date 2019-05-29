893673c25aeae9cb1f4ff8d9f1288ed0d34e5cc9
Алина Манагадзе <agmanagadze_1@edu.hse.ru>
2019-05-29 23:47:49 +0500

    +оформление

--- taskk_4/Program.cs
+++ taskk_4/Program.cs
@@ -61,9 +61,10 @@ namespace taskk_4
                     temp = ((sum1[sum1.Length - 1 - y] + sum2[sum2.Length - 1 - y])+temp) / 10;
                 }
             }
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("100! = ");
+            Console.ResetColor();
             Show(digits);
-            Console.WriteLine();
-            Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000");
             Console.ReadKey();
         }
     }
